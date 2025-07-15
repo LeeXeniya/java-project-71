2244c628777bc26f04e78e2601889b5d038ef894
Xeniya <leexenya@gmail.com>
2025-07-15 17:07:33 +0300

    added gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+    id("checkstyle")
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
